80fb926d96cf94202486bc3cca62f417bd373fc6
Bafana Baloyi <baloyibafana177@Gmail.com>
2025-04-18 18:23:54 +0200

    10th commit

---
+++ my-moodify-app/src/utilities/firebase-client.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBEqV-XFIOqKo17SQ_BfSFeBZNIERZVogU",
+    authDomain: "moodify-app-za.firebaseapp.com",
+    projectId: "moodify-app-za",
+    storageBucket: "moodify-app-za.firebasestorage.app",
+    messagingSenderId: "1087099817512",
+    appId: "1:1087099817512:web:c2b87dfd461e7525db2f1d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
---
+++ scripts/chat-gpt-prompt.md
@@ -0,0 +1,3 @@
+want to use fb (client-side) emulator with jest for testing. 
+I need detailed step by step instructions https://firebase.google.com/docs/emulator-suite/connect_and_prototype 
+section 1 start with fb and then section 2 do jest
